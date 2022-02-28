dc7945792ddcb908077c9ca38f192e1c4613c340
Keagan McClelland <keagan.mcclelland@gmail.com>
2022-02-28 14:45:57 -0700

    adds mount to migration

--- migration-from-lt-2-3-9.sh
+++ migration-from-lt-2-3-9.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 PATH_MD5=$(echo -n /mnt/filebrowser | md5sum | head -c 32)
 sqlite3 /media/photoview.db "update albums set path = '/mnt/filebrowser', path_hash = '$PATH_MD5' where id = 1;"
+echo '{"configured":false}'
